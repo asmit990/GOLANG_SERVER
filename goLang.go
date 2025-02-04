5c9c5ebecf63da166d820d6474165fd803a9bca1
asmit990 <asmitpandeyw4354@gmail.com>
2025-02-05 02:54:24 +0530

    Auto-commit: $(date)

--- goLang.go
+++ goLang.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main(){
-	
+
+}
+
+func PerformGetRequest(){
+	const myurl = "http://localhost:8000/get"
+
+	response, err := http.Get(myurl)
+	if err != nil{
+        panic(err)
+	}
+	defer response.Body.Close()
+
+	fmt.Println("Status code:", response.StatusCode)
+	fmt.Println("content length is ; ", response.ContentLength)
+
+	content, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
 }
